b44a58c5ca6719ceeb78b0bf41e770a937587ffc
mcatm <tkcs@pelepop.com>
2018-08-01 22:35:24 +0900

    info pages

---
+++ www/html/info_/index.php
@@ -0,0 +1,281 @@
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_meta.inc.php");?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_linkfile.inc.php");?>
+	
+	<!-- info str -->
+	<link rel="stylesheet" href="/ex/css/info.css" type="text/css" />
+	<link rel="stylesheet" href="/ex/css/info-320.css" media="screen and (max-width: 480px)" />
+	<!-- info end -->
+	
+	<title>会社概要 | UPLINK</title>
+	<meta property="og:image" content="/ex/img/uplink_img_og.gif" />
+	<meta name="Description" content="アップリンクの会社概要" />
+	<meta name="Keywords" content="アップリンク,UPLINK,映画,ビデオ,アート,書籍,シネマ,映像,フィルム,インディペンデント" />
+	
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_analytics.inc.php");?>
+
+</head>
+<body>
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_head.inc.php");?>
+<!-- pagewidth str -->
+<div id="pagewidth">
+	<!-- header str -->
+	<header>
+		<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_header_nav.inc.php");?>
+	</header>
+	<!-- header end -->
+	
+	<!-- container str -->
+	<div id="container" class="clearfix info rental">
+		<!-- maincontents str -->
+		<div id="maincontents">
+			<nav id="pan">
+				<p><a href="/">ホーム</a> &#8250; <em>会社概要</em> </p>
+			</nav>
+			
+			<!-- post str -->
+			<article class="clearfix post">
+				<header>
+					<h1>会社概要</h1>
+				</header>
+				<section class="body">
+					
+					<div class="r">
+						<img src="../ex/img/img_uplink_logo.gif" width="230" height="230" alt="UPLINKロゴ" class="logo" />
+					</div>
+					
+<p>UPLINKの意味は衛星と地上基地の通信を指します。地上基地から衛星へ電波を飛ばす事をUPLINK。衛星から地上基地へ電波を飛ばす事をDOWNLINKといいます。</p>
+
+					<p>ロゴの×印は、単純なまっすぐの×ではなく、忍者の巻き物、車のサスペンション、パン生地を延ばすローラーのような形をしています。X自体には未知の意味 が有り、『遊星からの物体X』『マルコムX』のように使われています。映画のレイティングでXといえばポルノを指し、親しい人に書く手紙の最後のXはキス の意味もあります。</p>
+					
+					<p>ロゴデザインはアップリンクの映画ポスターのデザインを多く手掛けているIt Is Designの伊丹友広氏が行いました。</p>
+					
+					<br class="clear" />
+					
+					<table class="simple">
+						<tr>
+							<th scope="row">会社名</th>
+							<td>有限会社アップリンク</td>
+						</tr>
+						<tr>
+							<th scope="row">役員</th>
+							<td>取締役 浅井 隆</td>
+						</tr>
+						<tr>
+							<th scope="row">設立</th>
+							<td>1987年9月10日</td>
+						</tr>
+						<tr>
+							<th scope="row">資本金</th>
+							<td>1,000万円</td>
+						</tr>
+						<tr>
+							<th scope="row">事業内容</th>
+							<td>
+								<ol>
+									<li>映画、映像ソフト、テレビ放映権の取得及び販売</li>
+									<li>映画、映像ソフトの企画、製作、興行</li>
+									<li>書籍、雑誌、小冊子の編集、出版</li>
+									<li>音楽、映像ソフトの製作販売</li>
+									<li>催事の企画</li>
+									<li>劇場、レストランなどの経営及び営業</li>
+									<li>前各号に関する一切の事業</li>
+								</ol>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">所在地</th>
+							<td>〒150-0042 東京都渋谷区宇田川町37-18</td>
+						</tr>
+					</table>
+					
+					<!-- contents str -->
+					<a name="rental1" id="rental1"></a>
+					<section class="contents">
+						<h2>沿革</h2>
+						
+						<dl class="chronology clearfix">
+							<dt>1987</dt>
+							<dd>
+								<h3>会社設立</h3>
+								<p>映画の配給会社としてスタート。<br />
+								最初の配給作品はデレク・ジャーマン監督の『エンジェリック・カンヴァセーション』。その後、映画の配給に留まらず、配給作品をビデオ化し独自ルートでの 販売を行う。またイギリスのテレビ局BBC、チャンネル4などと共に、ジャーマン監督の『ザ・ガーデン』『エドワードII』『ヴィトゲンシュタイン』 『BLUE』の共同製作を行う。映画以外では、テレビ・ドキュメンタリー、連続テレビドラマ（CX『90日間トテナム・パブ』）などを手がける。</p>
+							</dd>
+							
+							
+							<dd>
+								<h3>雑誌『骰子』刊行</h3>
+								<p>映画・音楽・アートなどを扱った隔月刊誌。2000年3月まで発行</p>
+							</dd>
+							
+							<dt>1993</dt>
+							<dd>
+								<h3>UPLINK FACTORYオープン</h3>
+								<p>映画の上映、イベントなどを行うスペースとして誕生。</p>
+							</dd>
+							
+							<dt>1995</dt>
+							<dd>
+								<h3>映画『愛の悪魔』（監督：ジョン・メイブリィ、音楽：坂本龍一）をBBCと共同製作</h3>
+								<p>カンヌ国際映画祭の"ある視点"部門に正式出品。</p>
+							</dd>
+							
+							<dt>2000</dt>
+							<dd>
+								<h3>映画『I.K.U.』（監督：シューリー・チェン）を製作</h3>
+								<p>サンダンス映画祭を皮切りに世界中の20以上の映画祭で上映。</p>
+								<h3>映画『ふたりの人魚』（監督：ロウ・イエ）を共同製作</h3>
+							</dd>
+							
+							<dt>2003</dt>
+							<dd>
+								<h3>映画『アカルイミライ』（監督：黒沢清）を製作</h3>
+								<p>カンヌ国際映画祭の"コンペティション"に出品。</p>
+								<h3>UPLINK GALLERYを早稲田鶴巻町にオープン（2005年に渋谷に移転）</h3>
+								<p>『"ロバート・メイプルソープ写真集"展』などを行う。</p>
+							</dd>
+							
+							<dt>2004</dt>
+							<dd>
+								<h3>UPLINK Xオープン</h3>
+								<p>日本一小さな映画館として誕生。</p>
+							</dd>
+							
+							<dt>2005</dt>
+							<dd>
+								<h3>アップリンク、渋谷宇田川町に移転</h3>
+								<p>アップリンク・ファクトリー、アップリンクX、アップリンク・ギャラリー、タベラが集結し、多目的なスペースとなってヴァージョンアップ。</p>
+							</dd>
+							
+							<dt>2006</dt>
+							<dd>
+								<h3>映画『甲野善紀身体操作術』（監督：藤井謙二郎）製作</h3>
+								<h3>映画『ストロベリーショートケイクス』（監督：矢崎仁司）製作</h3>
+							</dd>
+							
+							<dt>2007</dt>
+							<dd>
+								<h3>創立20周年上映イベント『音楽ドキュメンタリー映画祭』を開催</h3>
+							</dd>
+							
+							<dt>2008</dt>
+							<dd>
+								<p>映画『モンゴル』（監督：セルゲイ・ボドロフ）製作補助<br />
+								アカデミー賞外国語映画部門ノミネート</p>
+								<h3>カルチャー・ポータルSNSサイト<a href="http://www.webdice.jp/" target="_blank">『webDICE』</a>オープン</h3>
+                                <h3>震災後4月2日に映画『100,000年後の安全』を公開</h3>
+							</dd>
+							
+							<dt>2011</dt>
+							
+							
+							
+							<dd>
+								<h3>上映・イベントスペースUPLINK ROOMとMARKETをオープン</h3>
+							</dd>
+                            <dt>2012</dt>
+						</dl>
+					</section>
+					<!-- contents end -->
+					
+
+					<!-- contents str -->
+					<a name="rental2" id="rental2"></a>
+					<section class="contents">
+						<h2>紹介記事</h2>
+
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>kenbunden 見聞伝 (東京大学見聞伝ゼミナール)</h3>
+							<h4>Reports 2014駒場祭「ミニシネマ・パラダイス」講演会記事</h4>
+							<p>2014年11月23日に開催された駒場祭講演会「ミニシネマ・パラダイス」の様子<br />
+							<a class="arrow" target="_blank" href="http://kenbunden.net/general/archives/4956">記事を読む</a></p>
+						</section>
+						<!-- content end -->
+
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>フリーペーパー Kate</h3>
+							<h4>ロングインタビュー　浅井隆（アップリンク）</h4>
+							<p>「アップリンク設立にいたるまで」「映画配給をなぜ行うのか」「webDICEがなぜ今必要なのか」<br />
+							<a class="arrow" target="_blank" href="/ex/pdf/info01_kate.pdf">PDFをダウンロードする</a></p>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>log osaka web magazine</h3>
+							<h4><a href="http://www.log-osaka.jp/people/vol.9/ppl_vol9_1.html" target="_blank">people_vol.9 浅井隆（アップリンク主宰）</a></h4>
+							<p>社会転覆を目指す70年代サブカルチャーDNA<br />
+							<a class="arrow" target="_blank" href="/ex/pdf/info02_logosaka.pdf">PDFをダウンロードする</a></p>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>webacross</h3>
+							<h4>032 浅井隆</h4>
+							<p><a class="arrow" target="_blank" href="/ex/pdf/info03_across.pdf">PDFをダウンロードする</a></p>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>紀伊国屋書店</h3>
+							<h4>世界を均質化する力と戦うために　浅井隆</h4>
+							<p><a class="arrow" target="_blank" href="/ex/pdf/info04_kinokuniya.pdf">PDFをダウンロードする</a></p>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>Six Apart</h3>
+							<h4><a href="http://www.sixapart.jp/business/05675.html" target="_blank">『アップリンク』がMovable Typeを利用する理由</a></h4>
+							<p><a class="arrow" target="_blank" href="/ex/pdf/info05_sixapart.pdf">PDFをダウンロードする</a></p>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>港町キネマ通り</h3>
+							<h4><a href="http://www.cinema-st.com/mini/m035.html" target="_blank">アップリンク・ファクトリー</a></h4>
+							<p><a class="arrow" target="_blank" href="/ex/pdf/info06_cinema-st-factory.pdf">PDFをダウンロードする</a></p>
+
+							<h4><a href="http://www.cinema-st.com/mini/m032.html" target="_blank">アップリンクX</a></h4>
+							<p><a class="arrow" target="_blank" href="/ex/pdf/info07_cinema-st-x.pdf">PDFをダウンロードする</a></p>
+						</section>
+						<!-- content end -->
+					</section>
+					<!-- contents end -->
+					
+				</section>
+
+			</article>
+			<!-- post end -->
+		</div>
+		<!-- maincontents end -->
+				
+		<!-- sidearea str -->
+		<div id="sidearea">				
+			<aside class="box">
+				<ul class="categorys">
+					<li><a href="/info/">会社概要</a></li>
+					<li><a href="/news/">お知らせ</a></li>
+					<li><a href="/news/recruit">採用</a></li>
+					<li><a href="/info/privacypolicy/">個人情報の取り扱い</a></li>
+					<li><a href="/info/information_en/">English information</a></li>
+				</ul>
+			</aside>
+		</div>
+		<!-- sidearea end -->
+	</div>
+	<!-- container end -->
+</div>
+<!-- pagewidth end -->
+<!-- footer str -->
+<footer id="footer-nav">
+	<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_footer_nav.inc.php");?>
+</footer>
+<!-- footer end -->
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_foot.inc.php");?>
+</body>
+</html>
---
+++ www/html/info_/rental/index.php
@@ -0,0 +1,458 @@
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_meta.inc.php");?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_linkfile.inc.php");?>
+	
+	<!-- info str -->
+	<link rel="stylesheet" href="/ex/css/info.css" type="text/css" />
+	<link rel="stylesheet" href="/ex/css/info-320.css" media="screen and (max-width: 480px)" />
+	<!-- info end -->
+	
+	<title>施設レンタル・企画募集 | UPLINK</title>
+	<meta property="og:image" content="http://www.uplink.co.jp/ex/img/uplink_img_og.gif" />
+	<meta name="Description" content="アップリンク施設レンタル・企画募集" />
+	<meta name="Keywords" content="アップリンク,UPLINK,映画,ビデオ,アート,書籍,シネマ,映像,フィルム,インディペンデント" />
+	
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_analytics.inc.php");?>
+
+</head>
+<body>
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_head.inc.php");?>
+<!-- pagewidth str -->
+<div id="pagewidth">
+	<!-- header str -->
+	<header>
+		<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_header_nav.inc.php");?>
+	</header>
+	<!-- header end -->
+	
+	<!-- container str -->
+	<div id="container" class="clearfix info rental">
+		<!-- maincontents str -->
+		<div id="maincontents">
+			<nav id="pan">
+				<p><a href="/">ホーム</a> &#8250; <em>施設レンタル・企画募集</em> </p>
+			</nav>
+			
+			<!-- post str -->
+			<article class="clearfix post">
+				<header>
+					<h1>施設レンタル・企画募集</h1>
+				</header>
+				<section class="body">
+					<p>渋谷駅から徒歩7分という好立地のアップリンクでイベントや展示会、ライブ、パーティーを企画しませんか。映画館、ギャラリー、イベントスペース、カフェレストランなどのアップリンクの全ての施設をご利用いただけます。利用の方法は、下記の4つがあります。</p>
+					
+					<ol>
+						<li><a href="#rental1">施設レンタル</a></li>
+						<li><a href="#rental2">共同企画</a></li>
+						<li><a href="#rental3">OPEN FACTORY</a></li>
+						<li><a href="#rental4">OPEN GALLERY</a></li>
+					</ol>
+					
+					<!-- contents str -->
+					<a name="rental1" id="rental1"></a>
+					<section class="contents">
+						<h2>1. 施設レンタル</h2>
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>1.1 UPLINK FACTORY　<small>&#8250; <a href="/info/floorguide/">施設案内はこちら</a></small></h3>
+							
+							<section>
+								<h4>試写会場として　<small>※準備、会場の撤収を含む</small></h4>
+								<table class="simple">
+									<tr>
+										<th scope="row" class="day">平日</th>
+										<td class="time">30min</td>
+										<td>15:00～17:00</td>
+										<td class="charge">5,000円（+消費税）</td>
+									</tr>
+								</table>
+							</section>
+							
+							<section>
+								<h4>イベント、展示の会場として　<small>※準備、会場の撤収を含む</small></h4>
+								<table class="simple">
+									<tr>
+										<th scope="row" class="day">平日</th>
+										<td class="time">1h</td>
+										<td>15:00～22:00</td>
+										<td class="charge">16,000円（+消費税）<br />
+										<small class="blue">4時間以上のご利用をお願いします</small></td>
+									</tr>
+									<tr>
+										<th scope="row" class="day">土日祝日</th>
+										<td class="time">1h</td>
+										<td>15:00～22:00</td>
+										<td class="charge">16,000円（+消費税）<br />
+										<small class="blue">5時間以上のご利用をお願いします</small></td>
+									</tr>
+								</table>
+								
+							</section>
+							
+							<section>
+								<h4>施設・機材　<small>※使用料はレンタル料に含む</small></h4>
+								<ul>
+									<li>椅子58席</li>
+									<li>DLPプロジェクター、120インチスクリーン</li>
+									<li>デジタル上映設備一式（DCP、Blu-ray、DVD、HDVcam、DVcam、VHS）</li>
+									<li>JBLスピーカー5.1サラウンドシステム、THX</li>
+									<li>田口製PAシステム、16chミキサー、マイク5本、マイクスタンド、譜面台</li>
+									<li>光ファイバーインターネット環境</li>
+								</ul>
+							</section>
+						</section>
+						<!-- content end -->
+						
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>1.3 Tabela（カフェ・レストラン）</h3>
+							<p>テラス席を含めて38席。貸し切りでパーティ、結婚式の2次会などでのご利用が可能です。お気軽にご相談ください。<br />
+							<a class="arrow" target="_blank" href="/tabela/">タベラ公式サイト</a></p>
+						</section>
+						<!-- content end -->
+
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>1.4 複数の施設を同時にレンタルも可能</h3>
+							<p>レストラン「タベラ」と「ファクトリー」の間にある鉄の壁を移動することで多様な演出が可能になります。全フロアを使っての使用も可能。料金はお問い合わせください。</p>	
+							<section>
+								<h4>収容人員の目安</h4>
+								<ul>
+									<li>ファクトリーで上映やライブイベントを行う場合は最大58席</li>
+									<li>ファクトリー＋ギャラリーでイベントを行う場合、70人位が滞留可能</li>
+									<li>全スペース（ファクトリー＋タベラ＋ギャラリー）でパーティを行う場合90人が滞留可能</li>
+								</ul>
+							</section>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>1.5 申し込み</h3>
+							<p>連絡先、希望期日、イベント内容を明記の上、下記アドレスにメールをお送りください。<br />
+							<a href="mailto:factory@uplink.co.jp">factory@uplink.co.jp</a></p>
+							
+							<section>
+								<h4>お問い合わせ</h4>
+								<p>不明な点などお気軽にご相談ください。<br />
+								<a href="mailto:factory@uplink.co.jp">factory@uplink.co.jp</a>　tel. 03-6825-5503（担当：倉持、石井）</p>
+							</section>
+						</section>
+						<!-- content end -->
+					</section>
+					<!-- contents end -->
+					
+
+					<!-- contents str -->
+					<a name="rental2" id="rental2"></a>
+					<section class="contents">
+						<h2>2. 共同企画</h2>
+						<p>アップリンクと一緒に企画を練っていきイベントを実現するスタイルです。共同企画ですので施設使用料は頂かず、イベント後売り上げをアップリンクと企画者で分配します。</p>
+						<p>例）映画祭、上映会、ライブ、演劇、ダンス、トークショー、ワークショップ、展示、各種教室など。</p>
+						
+						<section>
+							<h4>お問い合わせ</h4>
+							<p><a href="mailto:factory@uplink.co.jp">factory@uplink.co.jp</a>　tel. 03-6825-5503（担当：倉持、石井）</p>
+						</section>
+					</section>
+					<!-- contents end -->
+					
+					
+					<!-- contents str -->
+					<a name="rental3" id="rental3"></a>
+					<section class="contents">
+						<h2>3. OPEN FACTORY</h2>
+						<p>クリエイター、映画製作者、イベンター、ミュージシャン、NPOなどの活動をサポートする趣旨で、お得に<a href="/info/floorguide/">UPLINK FACTORY</a>を使用いただける「オープン・ファクトリー」を実施しています。</p>
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>3.1 貸出日と応募締切　<small>随時更新</small></h3>
+							
+							<table class="simple">
+
+								<tr>
+									<th>貸出日</th>
+									<th class="deadline">応募締切</th>
+								</tr>
+
+								<tr>
+									<td>2018年7月31日（火）</td>
+									<td class="deadline">2018年6月18日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年9月3日（月）</td>
+									<td class="deadline">2018年7月30日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年9月10日（月）</td>
+									<td class="deadline">2018年8月6日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年9月11日（火）</td>
+									<td class="deadline">2018年8月6日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年9月25日（火）</td>
+									<td class="deadline">2018年8月20日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年10月9日（火）</td>
+									<td class="deadline">2018年8月27日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年10月15日（月）</td>
+									<td class="deadline">2018年9月3日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年10月22日（月）</td>
+									<td class="deadline">2018年9月10日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年10月23日（火）</td>
+									<td class="deadline">2018年9月10日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年10月29日（月）</td>
+									<td class="deadline">2018年9月18日（火）</td>
+								</tr>
+								<tr>
+									<td>2018年11月5日（月）</td>
+									<td class="deadline">2018年9月25日（火）</td>
+								</tr>
+								<tr>
+									<td>2018年11月6日（火）</td>
+									<td class="deadline">2018年9月25日（火）</td>
+								</tr>
+								<tr>
+									<td>2018年11月12日（月）</td>
+									<td class="deadline">2018年10月1日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年11月19日（月）</td>
+									<td class="deadline">2018年10月9日（火）</td>
+								</tr>
+								<tr>
+									<td>2018年11月20日（火）</td>
+									<td class="deadline">2018年10月9日（火）</td>
+								</tr>
+								<tr>
+									<td>2018年11月26日（月）</td>
+									<td class="deadline">2018年10月15日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年12月3日（月）</td>
+									<td class="deadline">2018年10月22日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年12月4日（火）</td>
+									<td class="deadline">2018年10月22日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年12月10日（月）</td>
+									<td class="deadline">2018年10月29日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年12月17日（月）</td>
+									<td class="deadline">2018年11月5日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年12月18日（火）</td>
+									<td class="deadline">2018年11月5日（月）</td>
+								</tr>
+								<tr>
+									<td>2018年12月24日（月）</td>
+									<td class="deadline">2018年11月12日（月）</td>
+								</tr>
+
+							</table>
+
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>3.2 使用条件など</h3>
+							<section>
+								<h4>利用時間</h4>
+								<p>18:00～22:30 ※準備、会場の撤収を含む</p>
+							</section>
+							
+							<section>
+								<h4>使用条件</h4>
+								<ul class="important">
+									<li>施設のレンタル代は無料</li>
+									<li>入場者全員に1ドリンクチケット（500円）を必ず購入いただきます</li>
+									<li>上映設備を使用する場合：使用料5,000円（+消費税）</li>
+									<li>音響設備を使用する場合：使用料5,000円（+消費税）</li>
+									<li>必ず事前に渋谷アップリンクで打ち合わせできる方</li>
+								</ul>
+							</section>
+							
+							<section>
+								<h4>預かり金・キャンセル代</h4>
+								<p>レンタル代は無料ですが、キャンセル代10,000円を預かり金として申し込み時に預けていただきます。当日イベント終了時に預かり金は返金します。キャンセルがあった場合は預かり金をキャンセル料とします。</p>
+							</section>
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>3.3 注意事項</h3>
+							<section>
+								<h4>1 宣伝について</h4>
+								<p>フライヤー、チラシなどの作成、配布、その他の宣伝に関しましては会場をレンタルされる方が行なってください。UPLINKが毎月発行しているフリーペーパー、UPLINK information冊子(月一回2万部発行)での告知、UPLINK FACTORYのホームページでの告知は会場側がいたします。</p>
+							</section>
+							
+							<section>
+								<h4>2 ライブ演奏について</h4>
+								<p>音楽のライブ演奏を行うことは可能です。楽器など詳細につきましては、その都度スタッフと相談してください。</p>
+							</section>
+							
+							<section>
+								<h4>3 準備、リハーサルについて</h4>
+								<p>スピーカー、椅子などの移動に関しても同様に、準備、撤去を含め、オープンファクトリーの貸し出し時間内で行うのであれば可能です。ただし、場内にあるものを外に運び出す事はできません。移動に関する人手はレンタルされる方が準備してください。会場をレンタルされる日、時間以外でのリハーサルはお断りいたします。</p>
+							</section>
+							
+							<section>
+								<h4>4 入場料金について</h4>
+								<p>入場料は700円とした場合は、500円はまずアップリンクにドリンク代として払っていただきます。</p>
+							</section>
+							
+							<section>
+								<h4>5 有料イベントの場合</h4>
+								<p>入場料を取ってイベントを行うことは可能です。ただし、事前打ち合せで機材使用料などの経費を確認させていただき、経費を超えた入場料があった場合は、主催者とアップリンクで折半させていただきます。</p>
+							</section>
+						</section>
+						<!-- content end -->
+						
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>3.4 応募方法</h3>
+							<p>件名を<strong>【オープン・ファクトリー予約/希望日】</strong>とし、本文にて下記「応募要項」をご明記の上ご応募下さい。企画書を添付して頂いても構いませんが、その際も「応募要項」は必ずご明記下さい。<br />
+							<a href="mailto:factory@uplink.co.jp">factory@uplink.co.jp</a></p>
+							
+							<p>締切日までに届いた企画を応募多数の場合は、内容を検討させていただいた後、締切日の翌日にメールでご連絡します。こちらからご連絡を差し上げた後、顔を合わせてのミーティングを渋谷アップリンクでさせていただきます。応募は必ず事前に打ち合わせできる方に限らせていただきます。</p>
+							
+							<section>
+								<h4>応募要項</h4>
+								<p>下記、（10）以外は必須項目となります。未確定情報の場合は「(仮)」とご明記下さい。</p>
+								
+								<p class="frame">
+								（1）お名前、住所、電話番号、メールアドレス、ホームページ<br />
+								（2）希望日（第三希望まで明記必須）<br />
+								（3）イベント名<br />
+								（4）イベント内容<br />
+								（5）イベントのタイムテーブル（準備、開場/開演時間、完全撤収時間含）<br />
+								（6）入場料金の設定<br />
+								（7）宣伝計画、集客/動員予測 <span class="notes">※a</span><br />
+								（8）上映設備使用の有無 <span class="notes">※b</span><br />
+								（9）音響設備使用の有無 <span class="notes">※c</span><br />
+								（10）参考資料（過去のイベント資料やURL等をご明記下さい）
+								</p>
+								
+								<p><span class="notes">※a</span> どのくらいのお客様がご来場されるか見当がついている場合はそちらをご明記下さい。見当がついていない場合は、どのようにお客様を集めるか、どのように宣伝をして集客を図るかの内容と、それを実践した結果どのくらいの集客が見込めるかの予測をご明記下さい。<br />
+								<span class="notes">※b</span> UPLINK FACTORYの機材を使用して、プロジェクターからスクリーンへの映写が可能。<br />
+								<span class="notes">※c</span> ライブイベントやマイクの使用の有無。</p>
+							</section>
+							
+							<section>
+								<h4>お問い合わせ</h4>
+								<p>不明な点などお気軽にお気軽にお電話にてお問い合わせください。<br />
+								tel. 03-6825-5503（担当：倉持、石井）</p>
+							</section>
+							
+						</section>
+						<!-- content end -->
+						
+					</section>
+					<!-- contents end -->
+					
+					
+					<!-- contents str -->
+					<a name="rental4" id="rental4"></a>
+					<section class="contents">
+						<h2>4. OPEN GALLERY</h2>
+						<p>クリエイター、映画製作者、イベンター、ミュージシャン、NPOなどの活動をサポートする趣旨で、無料で<a href="/info/floorguide/">UPLINK GALLERY</a>を使用いただける「オープン・ギャラリー」を実施しています。</p>
+
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>4.1 応募される方へ</h3>
+							<p>このギャラリーでは、いわゆるアート作品の展示から、アパレルなどの商品の展示会、フリーマーケット、期間限定ショップなど、使い方は自由です。使用料は無料（販売する場合は手数料を30％頂きます）ですが、応募される方は大原則として、ネットで無料のギャラリーを見つけたからと応募するのではなく、まず、現場を見に来てください。その上でアップリンクの活動を理解してくださり、カフェのタベラやイベントスペースのファクトリー、映画館Xと連動できる企画を優先的に採用します。2010年6月より、アート作品を含む全ての企画の選択、キュレーションを僕が行います。アート作品の選定の基準は、抽象的ですがプリミティブな感情から生み出されたもので、知的で、強くて、そしてなによりも美しいものとします。企画内容により利用期間を1週間か2週間に決めさせてもらいます。皆さんの応募をお待ちします。（アップリンク主宰　浅井隆）</p>
+							
+						</section>
+						<!-- content end -->
+						
+						<!-- content str -->
+						<section class="content clearfix">
+							<h3>4.2 応募方法</h3>
+							<p>応募要項を明記の上、指定の宛先まで企画書を郵送、或いはメールに添付してお送りください。</p>
+							
+							<section>
+								<h4>郵便</h4>
+								<p>〒150-0042 東京都渋谷区宇田川町37-18 トツネビル4階<br />
+								UPLINK「ギャラリー無料開放」係<br />
+								※企画書の返却は出来ませんので予めご了承ください</p>
+							</section>
+							
+							<section>
+								<h4>メール</h4>
+								<p><a href="mailto:factory@uplink.co.jp">factory@uplink.co.jp</a><br />
+								※件名を<strong>【UPLINK「ギャラリー無料開放」係】</strong>としてください。<br />
+								※企画書はword書類、若しくはpdf書類として作成してください。</p>
+							</section>
+							
+							<section>
+								<h4>応募要項</h4>
+								<p class="frame">
+								(1)お名前 <span class="notes">※</span><br />
+								(2)電話番号 <span class="notes">※</span><br />
+								(3)ご住所 <span class="notes">※</span><br />
+								(4)「何故」UPLINKで展示を行いたいか、その理由について簡潔に<br />
+								(5)「何を」展示したいか<br />
+								(6)「誰に」「どのように」展示をみせたいか、アイデアを簡潔に
+								</p>
+								
+								<p><span class="notes">※</span> 複数名での展示を希望される際は、代表者のお名前をお書きください</p>
+							</section>
+						</section>
+						<!-- content end -->
+						
+					</section>
+					<!-- contents end -->
+					
+				</section>
+
+			</article>
+			<!-- post end -->
+		</div>
+		<!-- maincontents end -->
+				
+		<!-- sidearea str -->
+		<div id="sidearea">				
+			<aside class="box">
+				<ul class="categorys">
+					<li><a href="/info/map/">地図・お問合せ</a></li>
+					<li><a href="/info/floorguide/">施設案内</a></li>
+					<li><a href="/info/rental/">施設レンタル・企画募集</a></li>
+					<li><a href="/info/pro/">プロサービス</a></li>
+					<li><a href="/info/member/">UPLINK会員制について</a></li>
+					<li><a href="/info/mailmagazine/">メールマガジン登録</a></li>
+					<li><a href="/info/twitter/">Twitterアカウント</a></li>
+				</ul>
+			</aside>
+		</div>
+		<!-- sidearea end -->
+	</div>
+	<!-- container end -->
+</div>
+<!-- pagewidth end -->
+<!-- footer str -->
+<footer id="footer-nav">
+	<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_footer_nav.inc.php");?>
+</footer>
+<!-- footer end -->
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_foot.inc.php");?>
+</body>
+</html>
