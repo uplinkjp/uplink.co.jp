5ffacd7f4537f4b582d00d837a52faa157a1682d
mcatm <tkcs@pelepop.com>
2018-08-06 21:51:39 +0900

    set page templates.

--- www/html/wp/wp-content/themes/uplink/page-info-twitter.php
+++ www/html/wp/wp-content/themes/uplink/page-info-twitter.php
@@ -1,1 +1,146 @@
 <?php /* Template Name: info/twitter */?>
+
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_meta.inc.php");?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_linkfile.inc.php");?>
+  
+  <!-- info str -->
+  <link rel="stylesheet" href="/ex/css/info.css" type="text/css" />
+  <link rel="stylesheet" href="/ex/css/info-320.css" media="screen and (max-width: 480px)" />
+  <!-- info end -->
+  
+  <title>Twitterアカウントの案内 | UPLINK</title>
+  <meta property="og:image" content="http://www.uplink.co.jp/ex/img/uplink_img_og.gif" />
+  <meta name="Description" content="アップリンクが運営するTwitterアカウントの案内" />
+  <meta name="Keywords" content="アップリンク,UPLINK,映画,ビデオ,アート,書籍,シネマ,映像,フィルム,インディペンデント" />
+  
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_head_analytics.inc.php");?>
+
+</head>
+<body>
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_head.inc.php");?>
+<!-- pagewidth str -->
+<div id="pagewidth">
+  <!-- header str -->
+  <header>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_header_nav.inc.php");?>
+  </header>
+  <!-- header end -->
+  
+  <!-- container str -->
+  <div id="container" class="clearfix info">
+    <!-- maincontents str -->
+    <div id="maincontents">
+      <nav id="pan">
+        <p><a href="/">ホーム</a> &#8250; <em>Twitterアカウントの案内</em> </p>
+      </nav>
+      
+      <!-- post str -->
+      <article class="clearfix post">
+        <header>
+          <h1>Twitterアカウントの案内</h1>
+        </header>
+        <section class="body">
+          <p>アップリンクではTwitterで最新情報を配信しています。アカウントをお持ちの方はフォローしていただくことで、情報を取得することが可能です。<br />
+          <a href="http://twitter.com" target="_blank" class="arrow">twitter.com</a></p>
+          
+          
+          <section class="tw-accounts">
+            <h2>総合アカウント</h2>
+            <section class="tw-account clearfix">
+              <figure>
+                <a href="https://twitter.com/uplink_jp" target="_blank"><img src="http://furyu.nazo.cc/twicon/uplink_jp/bigger" width="73" height="73"></a>
+              </figure>
+              <p><a href="https://twitter.com/uplink_jp" class="twitter-follow-button" data-show-count="true" data-lang="ja" data-size="large">@uplink_jpをフォロー</a><br />
+              アップリンク全体の総合アカウントです。配給作品、DVD、イベントなど様々な情報を配信。</p>
+            </section>
+  
+            <section class="tw-account clearfix">
+              <figure>
+                <a href="https://twitter.com/webdice" target="_blank"><img src="http://furyu.nazo.cc/twicon/webdice/bigger" width="73" height="73"></a>
+              </figure>
+              <p><a href="https://twitter.com/webdice" class="twitter-follow-button" data-show-count="true" data-lang="ja" data-size="large">@webdiceをフォロー</a>
+              <br />
+              アップリンクが運営するカルチャーポータルサイト<a href="http://www.webdice.jp/" target="_blank">「webDICE」</a>の新着記事を配信。</p>
+            </section>
+          </section>
+          
+          <section class="tw-accounts"> 
+            <h2>施設アカウント</h2>
+            
+            <section class="tw-account clearfix">
+              <figure>
+                <a href="https://twitter.com/uplink_shibuya" target="_blank"><img src="http://furyu.nazo.cc/twicon/uplink_shibuya/bigger" width="73" height="73"></a>
+              </figure>
+              <p><a href="https://twitter.com/uplink_shibuya" class="twitter-follow-button" data-show-count="true" data-lang="ja" data-size="large">@uplink_shibuyaをフォロー</a>
+              <br />
+              アップリンク渋谷のアカウント</p>
+            </section>
+            
+            <section class="tw-account clearfix">
+              <figure>
+                <a href="https://twitter.com/uplink_joji" target="_blank"><img src="http://furyu.nazo.cc/twicon/uplink_joji/bigger" width="73" height="73"></a>
+              </figure>
+              <p><a href="https://twitter.com/uplink_joji" class="twitter-follow-button" data-show-count="true" data-lang="ja" data-size="large">@uplink_jojiをフォロー</a><br />
+              アップリンク吉祥寺のアカウント</p>
+            </section>
+            
+            <section class="tw-account clearfix">
+              <figure>
+                <a href="https://twitter.com/Tabela_jp" target="_blank"><img src="http://furyu.nazo.cc/twicon/Tabela_jp/bigger" width="73" height="73"></a>
+              </figure>
+              <p><a href="https://twitter.com/Tabela_jp" class="twitter-follow-button" data-show-count="true" data-lang="ja" data-size="large">@Tabela_jpをフォロー</a>
+  <br />
+              カフェレストラン<a href="http://www.uplink.co.jp/tabela/" target="_blank">Tabela（タベラ）</a>のアカウント</p>
+            </section>
+          </section>
+          
+
+          <section class="tw-accounts"> 
+            <h2>その他アカウント</h2>
+            <section class="tw-account clearfix">
+              <figure>
+                <a href="https://twitter.com/uplink_els" target="_blank"><img src="http://furyu.nazo.cc/twicon/uplink_els/bigger" width="73" height="73"></a>
+              </figure>
+              <p><a href="https://twitter.com/uplink_els" class="twitter-follow-button" data-show-count="true" data-lang="ja" data-size="large">@uplink_elsをフォロー</a>
+              <br />
+              アップリンクが配給する地球と生活を考える映画シリーズ、アップリンク・アースライフ・シリーズ公式アカウント</p>
+            </section>
+          </section>
+          
+
+          
+          
+        </section>
+
+      </article>
+      <!-- post end -->
+    </div>
+    <!-- maincontents end -->
+        
+    <!-- sidearea str -->
+    <div id="sidearea">       
+      <aside class="box">
+        <ul class="categorys">
+          <li><a href="/info/map/">地図・お問合せ</a></li>
+          <li><a href="/info/floorguide/">施設案内</a></li>
+          <li><a href="/info/rental/">施設レンタル・企画募集</a></li>
+          <li><a href="/info/pro/">プロサービス</a></li>
+          <li><a href="/info/member/">UPLINK会員制について</a></li>
+          <li><a href="/info/mailmagazine/">メールマガジン登録</a></li>
+          <li><a href="/info/twitter/">Twitterアカウント</a></li>
+        </ul>
+      </aside>
+    </div>
+    <!-- sidearea end -->
+  </div>
+  <!-- container end -->
+</div>
+<!-- pagewidth end -->
+<!-- footer str -->
+<footer id="footer-nav">
+  <?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_footer_nav.inc.php");?>
+</footer>
+<!-- footer end -->
+<?php require $_SERVER['DOCUMENT_ROOT'] . ("/ex/inc/_body_foot.inc.php");?>
+</body>
+</html>
