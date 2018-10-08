642d88ecfcd798546471f5c2232423168ff87435
uunnee <une327@gmail.com>
2018-10-09 00:51:14 +0900

    各トップフロント ( JS未実装 )

--- src/_dist/js/script.js
+++ src/_dist/js/script.js
@@ -6,12 +6,32 @@ NE = {
   init: function() {
     $( function() {
       NE.sample.init();
+      NE.bind();
+    });
+    $(window).on('load', function() {
+      NE.onload();
     })
-
   },
 
   bind: function() {
+    console.log('bind');
+
   },
+
+  onload: function() {
+    console.log('onload');
+    $('.js-slick').slick({
+      infinite: true,
+      dots:true,
+      slidesToShow: 1,
+      centerMode: true, //要素を中央寄せ
+      centerPadding:'6%', //両サイドの見えている部分のサイズ
+      prevArrow: '',
+      nextArrow: '',
+    });
+
+
+  }
 
 };
 
