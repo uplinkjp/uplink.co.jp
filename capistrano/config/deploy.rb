a6cc2a6a3fe09905c8d1e52293a6e138e4673584
mcatm <tkcs@pelepop.com>
2018-10-09 22:54:41 +0900

    bin.

---
+++ capistrano/config/deploy.rb
@@ -0,0 +1,62 @@
+# config valid only for current version of Capistrano
+lock "3.11.0"
+
+set :application, "cinra.dev"
+set :repo_url, "git@github.com:cinra/project-template.git"
+set :project_root, "/srv/"
+
+framework_tasks = [:starting, :started, :updating, :updated, :publishing, :published, :finishing, :finished]
+
+framework_tasks.each do |t|
+  Rake::Task["deploy:#{t}"].clear
+end
+
+Rake::Task[:deploy].clear
+
+task :init do
+  run_locally do
+    # execute "uptime"
+  end
+  on roles(:web) do
+    execute "cd #{fetch :project_root}; git clone #{fetch :repo_url} #{fetch :application}"
+  end
+end
+
+task :deploy do
+  on roles(:web) do
+    branch = capture "cd #{fetch :project_root}#{fetch :application}; git symbolic-ref --short HEAD"
+    execute "cd #{fetch :project_root}#{fetch :application}; git pull origin #{branch}"
+  end
+end
+
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
