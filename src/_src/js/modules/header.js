616e50f3e1f2d1d0e01e108682496b2e06c78da6
uunnee <une327@gmail.com>
2018-11-14 02:12:36 +0900

    サブナビまわり構造組み直し

--- src/_src/js/modules/header.js
+++ src/_src/js/modules/header.js
@@ -9,14 +9,32 @@ UPLINK.header = {
   $subNavTrigger: null,
   _st: 0,
 
+  isScroll: false,
+
+  $mainClone: null,
+  $subClone: null,
+
   isFrontPage: false,
   hasSubNavi: false,
 
   isMainNavOpen: false,
   isSubNavOpen: false,
 
+  bottom: 0,
+
   init: function() {
     UPLINK.header.$el = $('.js-headerWrap');
+
+    UPLINK.header.$mainClone = $('.l-nav').clone();
+    UPLINK.header.$mainClone.addClass('is-clone');
+    $('body').append(UPLINK.header.$mainClone);
+    $('.l-nav.is-clone').wrap('<div class="js-main-clone"></div>');
+
+    UPLINK.header.$mainClone = $('.l-nav_sub').clone();
+    UPLINK.header.$mainClone.addClass('is-clone');
+    $('body').append(UPLINK.header.$mainClone);
+    $('.l-nav_sub.is-clone').wrap('<div class="js-sub-clone"></div>')
+
 
     UPLINK.header.$main = $('.l-nav');
     UPLINK.header.$sub = $('.l-header_sub');
@@ -44,10 +62,6 @@ UPLINK.header = {
     UPLINK.header.bind();
   },
 
-  // fix時にガタッとするのを防ぐ
-  setSize: function() {
-    $('.js-header-wrap').height(UPLINK.header.$main_height);
-  },
 
   bind: function() {
     UPLINK.header.$mainNavTrigger.on('click', function() {
@@ -73,8 +87,9 @@ UPLINK.header = {
   },
 
   scroll: function(st) {
-    if(st > UPLINK.header.$main_height) {
+    if(st > 300) {
       $('body').addClass('is-scroll');
+      UPLINK.header.isScroll = true;
       UPLINK.header.closeMainNav();
       UPLINK.header.closeSubNav();
 
@@ -84,6 +99,7 @@ UPLINK.header = {
         $('body').removeClass('is-navfixed');
       }
     } else {
+      UPLINK.header.isScroll = false;
       $('body').removeClass('is-scroll');
       $('body').removeClass('is-navfixed');
 
@@ -95,28 +111,22 @@ UPLINK.header = {
 
   openMainNav: function() {
     UPLINK.header.isMainNavOpen = true;
-    // UPLINK.scrollBan.ban(UPLINK.header._st);
-    // UPLINK.header.$main.addClass('is-open');
     $('body').addClass('main-open');
-    UPLINK.header.$el.css({
-      'margin-top': '0px'
-    });
-
-    // UPLINK.header.$main.css({
-    //   'height': UPLINK.header.$main_height + 'px'
-    // });
-    // UPLINK.header.$sub.css({
-    //   'top': UPLINK.header.$main_height + 'px'
-    // });
-
+    if(!UPLINK.header.isScroll) {
+      UPLINK.header.$el.css({
+        'margin-top': '0px'
+      });
+    }
   },
 
   closeMainNav: function() {
     UPLINK.header.isMainNavOpen = false;
     $('body').removeClass('main-open');
-    UPLINK.header.$el.css({
-      'margin-top': - UPLINK.header.$main_height +'px'
-    });
+    if(!UPLINK.header.isScroll) {
+      UPLINK.header.$el.css({
+        'margin-top': - UPLINK.header.$main_height +'px'
+      });
+    }
 
     // UPLINK.scrollBan.release();
     // UPLINK.header.$main.css({
@@ -134,17 +144,28 @@ UPLINK.header = {
     // UPLINK.scrollBan.ban(UPLINK.header._st);
     // UPLINK.header.$sub.addClass('is-open');
     $('body').addClass('sub-open');
-    UPLINK.header.$sub.css({
-      'height': UPLINK.header.$sub_height + 'px'
+    UPLINK.header.bottom = Number($('.js-subNavTrigger').parent('p').css('bottom').replace('px',''));
+    $('.js-subNavTrigger').parent('p').css({
+      'bottom': UPLINK.header.bottom + UPLINK.header.$sub_height + 'px'
     });
+    if(!UPLINK.header.isScroll) {
+      UPLINK.header.$sub.css({
+        'height': UPLINK.header.$sub_height + 'px'
+      });
+    }
   },
 
   closeSubNav: function() {
     UPLINK.header.isSubNavOpen = false;
     // UPLINK.scrollBan.release();
-    UPLINK.header.$sub.css({
-      'height': 100+'px'
-    })
+    $('.js-subNavTrigger').parent('p').css({
+      'bottom': UPLINK.header.bottom + 'px'
+    });
+    if(!UPLINK.header.isScroll) {
+      UPLINK.header.$sub.css({
+        'height': 100+'px'
+      })
+    }
     // UPLINK.header.$sub.removeClass('is-open');
     $('body').removeClass('sub-open');
   },
