697cd071d9b515b0a7c44246f82a4355983a7777
uunnee <une327@gmail.com>
2018-11-17 03:31:36 +0900

    #45 sliderの調整

--- src/_dist/js/script.js
+++ src/_dist/js/script.js
@@ -230,28 +230,30 @@ UPLINK.scrollBan = {
 UPLINK.slick = {
 
   init: function() {
+
+    // console.log('aaa');
+    // var ww = $(window).outerWidth();
+    // var slideW = ( ww * 0.875 ) - 30 - 8;
+    // if( slideW >= 1280 ) slideW = 1280;
+    // var padding = ( ww - slideW ) / 2;
+
+
     UPLINK.slick.bind();
     $('.js-slick').slick({
       infinite: true,
       dots: true,
       slidesToShow: 1,
       centerMode: true, //要素を中央寄せ
-      centerPadding:'6%', //両サイドの見えている部分のサイズ
+      // centerPadding: padding + 'px', //両サイドの見えている部分のサイズ
       prevArrow: '',
       nextArrow: '',
+      variableWidth: true,
       autoplay: true,
       autoplaySpeed: 4000,
     });
   },
 
   bind: function() {
-    // console.log('bind');
-    // $('.js-slick').on('init', function(){
-    //   console.log('init');
-    //   setTimeout( function() {
-    //      $('.js-slider-dotdotdot').dotdotdot();
-    //   },100);
-    // });
   }
 
 };
