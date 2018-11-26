2c7a16d666f1d8dc6be61b5765920687960af4b0
mcatm <tkcs@pelepop.com>
2018-11-26 20:26:08 +0900

    hotfix.

--- www/html/wp/wp-content/themes/uplink_theater/partials/loop-news.php
+++ www/html/wp/wp-content/themes/uplink_theater/partials/loop-news.php
@@ -3,6 +3,6 @@
     <p class="list-news-date"><?php echo get_the_date( $d = 'Y.m.d' )?><?php if(is_new()):?><span>NEW</span><?php endif?></p>
     <p class="list-news-text"><?php the_title()?></p>
   </div>
-  <?php if( have_img() ):?><p class="list-news-thumb"><img src="<?php echo get_the_post_thumbnail_url( null, 'medium' )?>" alt="<?php echo htmlspecialchars(get_title())?>" /></p><?php endif?>
+  <?php if( have_img() ):?><p class="list-news-thumb"><img src="<?php echo get_the_post_thumbnail_url( null, 'medium' )?>" alt="<?php echo htmlspecialchars(get_the_title())?>" /></p><?php endif?>
   <?php /*p class="list-news-thumb"><img src="https://placehold.jp/337x177.png" alt="ハービー・山口写真展「今日は、映画を観に行く」撮影プロジェクトの被写体募集！" /></p */?>
 </a></li>
