7415b9bd566b0217385581542ee105b9f6669c67
mcatm <tkcs@pelepop.com>
2018-11-29 23:00:14 +0900

    page title.

---
+++ www/html/wp/wp-content/themes/uplink_theater/navs/detail-page.php
@@ -0,0 +1,12 @@
+<?php
+
+$title_en = get_field('title_en');
+
+?><div class="l-header_sub" style="height: 100px;">
+  <div class="header_sub-inner">
+    <h2 class="header_sub-heading">
+      <?php the_title()?>
+      <?php if( $title_en ):?><span><?php echo $title_en ?></span><?php endif?>
+    </h2>
+  </div>
+</div>
