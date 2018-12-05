5bf030fce7c70eafb838dab00a083515512fc9cf
mcatm <tkcs@pelepop.com>
2018-12-06 02:31:01 +0900

    search

--- www/html/wp/wp-content/themes/uplink_theater/partials/search.php
+++ www/html/wp/wp-content/themes/uplink_theater/partials/search.php
@@ -1,5 +1,5 @@
 <div class="form-googlesearch">
-  <form action="<?php home_url('search')?>">
+  <form action="<?php echo home_url('search')?>">
     <p class="form-googlesearch-text"><input type="text" name="q" value="" placeholder="Googleカスタム検索" /></p>
     <p class="form-googlesearch-submit"><input type="submit" /></p>
   </form>
