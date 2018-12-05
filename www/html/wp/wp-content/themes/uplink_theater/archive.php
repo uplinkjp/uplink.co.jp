9e1ba14f11261bfe490f85e6c4e8c1d60e6f1f69
mcatm <tkcs@pelepop.com>
2018-12-05 23:41:41 +0900

    sticky posts.

--- www/html/wp/wp-content/themes/uplink_theater/archive.php
+++ www/html/wp/wp-content/themes/uplink_theater/archive.php
@@ -1,4 +1,31 @@
-<?php get_template_part( 'partials/header' )?>
+<?php
+
+$args = $wp_query->query_vars;
+$sticky_posts = get_option( 'sticky_posts' );
+
+if ( $sticky_posts )
+{
+
+  if (!is_paged())
+  {
+    $args = array_merge( $args, array(
+      'post__in'              => $sticky_posts,
+      'posts_per_page'        => 9999,
+      'ignore_sticky_posts'   => 1
+    ));
+  }
+  else
+  {
+    $args = array_merge( $args, array(
+      'post__not_in' => $sticky_posts,
+    ));
+  }
+
+}
+
+$q = new WP_Query( $args );
+
+get_template_part( 'partials/header' )?>
 
 <div class="js-headerWrap">
   <?php get_template_part( 'navs/global', get_uplink_site() )?>
@@ -11,8 +38,8 @@
   <div class="list_archive">
 
   <?php
-  while( have_posts() ):
-  the_post();
+  while( $q->have_posts() ):
+  $q->the_post();
 
   get_template_part( 'partials/loop', 'panel' );
 
@@ -21,6 +48,9 @@
   </div>
 </section>
 
-<?php wp_paginate()?>
+<?php
+wp_reset_query();
+wp_paginate();
+?>
 
 <?php get_template_part( 'partials/footer' );
