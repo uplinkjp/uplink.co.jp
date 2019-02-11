a61b4e28b91a735c730e2550c800149eee6b443c
mcatm <tkcs@pelepop.com>
2019-02-11 20:49:17 +0900

    wip.

--- www/html/wp/wp-content/themes/uplink_theater/archive-news.php
+++ www/html/wp/wp-content/themes/uplink_theater/archive-news.php
@@ -1,4 +1,46 @@
-<?php get_template_part( 'partials/header' )?>
+<?php
+
+$args = $wp_query->query_vars;
+
+$sticky_posts = get_option( 'sticky_posts' );
+
+$sticky_q = null;
+
+if ( $sticky_posts )
+{
+
+  if (!is_paged())
+  {
+
+    $sticky_q = new WP_Query( array_merge( $args, array(
+      'post__in'              => $sticky_posts,
+      'posts_per_page'        => 9999,
+      'ignore_sticky_posts'   => 1
+    )) );
+
+    $posts_per_page = (int)$args['posts_per_page'] - $sticky_q->found_posts;
+    if ($posts_per_page < 1) $args['post_type'] = 'empty';
+
+    $args = array_merge( $args, array(
+      'post__not_in'          => $sticky_posts,
+      'posts_per_page'        => $posts_per_page,
+    ));
+
+  }
+  else
+  {
+    $args = array_merge( $args, array(
+      'post__not_in' => $sticky_posts,
+      'ignore_sticky_posts'   => 1,
+      // 'paged' => $args['paged'] - 1,
+    ));
+  }
+
+}
+
+$q = new WP_Query( $args );
+
+get_template_part( 'partials/header' )?>
 
 <div class="js-headerWrap">
   <?php get_template_part( 'navs/global', get_uplink_site() )?>
@@ -11,8 +53,19 @@
   <ul class="list-news">
 
   <?php
-  while( have_posts() ):
-  the_post();
+  if ($sticky_q):
+
+  while( $sticky_q->have_posts() ):
+  $sticky_q->the_post();
+
+  get_template_part( 'partials/loop', 'news' );
+
+  endwhile;
+  endif?>
+
+  <?php
+  while( $q->have_posts() ):
+  $q->the_post();
 
   get_template_part( 'partials/loop', 'news' );
 
